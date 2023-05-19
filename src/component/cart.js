c04190cb7122868513d007604c5f37bd560480c0
rohitgoyal9293 <58856105+rohitgoyal9293@users.noreply.github.com>
2023-05-19 17:02:38 +0530

    Delete src directory

--- src/component/cart.js
+++
@@ -1,9 +0,0 @@
-function Cart({checkoutList,checkout}) {
-  return (
-    <div className="cartContainer">
-      <span onClick={checkout} className="cart">Checkout {checkoutList.length} Items</span>
-    </div>
-  );
-}
-
-export default Cart;
